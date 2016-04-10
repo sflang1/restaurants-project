ad37237cccd43cd643f6d5d5d8e939860febae3b
Sebastián Landínez <slandinezg@gmail.com>
2016-04-09 19:04:44 -0500

    Generated users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :restaurants
   devise_for :users
+  root 'welcome#index'
+  get 'dashboard' => 'users#index', as: 'user_root'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409222518) do
+ActiveRecord::Schema.define(version: 20160409224816) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string   "name"
+    t.string   "type"
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
